2739cc98edc5fcc15f38b269b75981dffc8afa65
juanitagila2 <j.gila2@uniandes.edu.co>
2025-05-18 23:46:32 -0500

    coleccion postman

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Estudiante } from './entities/estudiante.entity';
+import { Proyecto } from './entities/proyecto.entity';
+import { Profesor } from './entities/profesor.entity';
+import { Evaluacion } from './entities/evaluacion.entity';
+import { EstudianteController } from './controllers/estudiante.controller';
+import { EstudianteService } from './services/estudiante.service';
+import { ProyectoController } from './controllers/proyecto.controller';
+import { ProyectoService } from './services/proyecto.service';
+import { ProfesorController } from './controllers/profesor.controller';
+import { ProfesorService } from './services/profesor.service';
+import { EvaluacionController } from './controllers/evaluacion.controller';
+import { EvaluacionService } from './services/evaluacion.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'parcial',
+      entities: [Estudiante, Proyecto, Profesor, Evaluacion],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Estudiante, Proyecto, Profesor, Evaluacion]),
+  ],
+  controllers: [
+    AppController,
+    EstudianteController,
+    ProyectoController,
+    ProfesorController,
+    EvaluacionController,
+  ],
+  providers: [
+    AppService,
+    EstudianteService,
+    ProyectoService,
+    ProfesorService,
+    EvaluacionService,
+  ],
 })
 export class AppModule {}
